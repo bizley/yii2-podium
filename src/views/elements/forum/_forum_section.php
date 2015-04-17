362ab512ed25589b67889df6db1248ac1f5e43c5
Bizley <pb@human-device.com>
2015-04-17 20:23:20 +0200

    0.0.32

--- src/views/elements/forum/_forum_section.php
+++ src/views/elements/forum/_forum_section.php
@@ -18,7 +18,16 @@ use yii\helpers\Url;
 </div>
 
 <div class="panel panel-default">
-    <div class="panel-body small text-right">
-        <a href="">RSS</a>
+    <div class="panel-body small">
+        <a href="" class="pull-right">RSS</a>
+        <ul class="list-inline">
+            <li class="text-muted"><button class="btn btn-xs"><span class="glyphicon glyphicon-comment"></span></button> <?= Yii::t('podium/view', 'No New Posts') ?></li>
+            <li class="text-muted"><button class="btn btn-xs"><span class="glyphicon glyphicon-leaf"></span></button> <?= Yii::t('podium/view', 'New Posts') ?></li>
+            <li class="text-muted"><button class="btn btn-xs"><span class="glyphicon glyphicon-fire"></span></button> <?= Yii::t('podium/view', 'Hot Thread') ?></li>
+            <li class="text-muted"><button class="btn btn-xs"><span class="glyphicon glyphicon-lock"></span></button> <?= Yii::t('podium/view', 'Locked Thread') ?></li>
+            <li class="text-muted"><button class="btn btn-xs"><span class="glyphicon glyphicon-pushpin"></span></button> <?= Yii::t('podium/view', 'Pinned Thread') ?></li>
+            <li class="text-muted"><button class="btn btn-success btn-xs">&nbsp;&nbsp;&nbsp;&nbsp;</button> <?= Yii::t('podium/view', 'New Posts') ?></li>
+            <li class="text-muted"><button class="btn btn-warning btn-xs">&nbsp;&nbsp;&nbsp;&nbsp;</button> <?= Yii::t('podium/view', 'Edited Posts') ?></li>
+        </ul>
     </div>
 </div>
--- src/views/elements/forum/_thread.php
+++ src/views/elements/forum/_thread.php
@@ -2,13 +2,35 @@
 
 use yii\helpers\Html;
 use yii\helpers\Url;
+use yii\web\View;
+
+$this->registerJs('jQuery(\'[data-toggle="tooltip"]\').tooltip();', View::POS_READY, 'bootstrap-tooltip');
+
+$btnClass = 'default';
+$icon     = 'comment';
+$iconDesc = 'No New Posts';
+
+if ($model->locked) {
+    $icon     = 'lock';
+    $iconDesc = 'Locked Thread';
+}
+elseif ($model->pinned) {
+    $icon     = 'pushpin';
+    $iconDesc = 'Pinned Thread';
+}
+elseif ($model->posts >= 20) {
+    $icon     = 'fire';
+    $iconDesc = 'Hot Thread';
+}
 
 ?><td>
+    <a href="<?= Url::to(['thread', 'cid' => $model->category_id, 'fid' => $model->forum_id, 'id' => $model->id, 'slug' => $model->slug]) ?>" class="pull-left btn btn-<?= $btnClass ?>" style="margin-right:10px" data-toggle="tooltip" data-placement="top" title="<?= Yii::t('podium/view', $iconDesc) ?>">
+        <span class="glyphicon glyphicon-<?= $icon ?>"></span>
+    </a>
     <a href="<?= Url::to(['thread', 'cid' => $model->category_id, 'fid' => $model->forum_id, 'id' => $model->id, 'slug' => $model->slug]) ?>" class="center-block">
-        <span class="glyphicon glyphicon-comment" style="padding-right:10px"></span>
         <?= Html::encode($model->name) ?>
     </a>
 </td>
-<td class="text-right"><?= $model->posts - 1 ?></td>
+<td class="text-right"><?= $model->posts > 0 ? $model->posts - 1 : 0 ?></td>
 <td class="text-right"><?= $model->views ?></td>
 <td><?= $model->getLatestPost() ?></td>
