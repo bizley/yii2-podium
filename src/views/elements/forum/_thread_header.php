80f0ed56f4107cf8cd0e9a9b11f43b609c5530a1
Bizley <pb@human-device.com>
2015-05-15 20:14:56 +0200

    0.0.61

--- src/views/elements/forum/_thread_header.php
+++ src/views/elements/forum/_thread_header.php
@@ -2,5 +2,5 @@
     <th class="col-sm-7"><small><?= Yii::t('podium/view', 'Thread') ?></small></th>
     <th class="col-sm-1 text-center"><small><?= Yii::t('podium/view', 'Replies') ?></small></th>
     <th class="col-sm-1 text-center"><small><?= Yii::t('podium/view', 'Views') ?></small></th>
-    <th class="col-sm-2"><small><?= Yii::t('podium/view', 'Latest Post') ?></small></th>
+    <th class="col-sm-3"><small><?= Yii::t('podium/view', 'Latest Post') ?></small></th>
 </tr>
--- src/views/elements/main/_breadcrumbs.php
+++ src/views/elements/main/_breadcrumbs.php
@@ -1,7 +1,16 @@
 <?php
 
 use yii\widgets\Breadcrumbs;
+?>
 
-echo Breadcrumbs::widget([
-    'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-]);
+<div class="row">
+    <div class="col-sm-9">
+        <?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : []]); ?>
+    </div>
+    <div class="col-sm-3">
+        <div class="input-group">
+            <input type="text" class="form-control">
+            <span class="input-group-addon"><span class="glyphicon glyphicon-search"></span>
+        </div>
+    </div>
+</div>
