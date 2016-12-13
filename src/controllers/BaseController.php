7c816354a16453e943e3947f67c22b38dd13bae9
Bizley <pawel@positive.codes>
2016-12-13 21:35:53 +0100

    base refactor

--- src/controllers/BaseController.php
+++ src/controllers/BaseController.php
@@ -48,27 +48,23 @@ class BaseController extends YiiController
      */
     public function beforeAction($action)
     {
-        if (parent::beforeAction($action)) {
-            $warnings = Yii::$app->session->getFlash('warning');
-
-            $maintenance = $this->maintenanceCheck($action, $warnings);
-            if ($maintenance !== false) {
-                return $maintenance;
-            }
-
-            $email = $this->emailCheck($warnings);
-            if ($email !== false) {
-                return $email;
-            }
-
-            $upgrade = $this->upgradeCheck($warnings);
-            if ($upgrade !== false) {
-                return $upgrade;
-            }
-
-            return true;
+        if (!parent::beforeAction($action)) {
+            return false;
         }
-        return false;
+        $warnings = Yii::$app->session->getFlash('warning');
+        $maintenance = $this->maintenanceCheck($action, $warnings);
+        if ($maintenance !== false) {
+            return $maintenance;
+        }
+        $email = $this->emailCheck($warnings);
+        if ($email !== false) {
+            return $email;
+        }
+        $upgrade = $this->upgradeCheck($warnings);
+        if ($upgrade !== false) {
+            return $upgrade;
+        }
+        return true;
     }
     
     /**
@@ -79,28 +75,17 @@ class BaseController extends YiiController
     public static function warnings()
     {
         return [
-            'maintenance' => Yii::t(
-                'podium/flash', 
-                'Podium is currently in the Maintenance mode. All users without Administrator privileges are redirected to {maintenancePage}. You can switch the mode off at {settingsPage}.', 
-                [
-                    'maintenancePage' => Html::a(Yii::t('podium/flash', 'Maintenance page'), ['forum/maintenance']),
-                    'settingsPage'    => Html::a(Yii::t('podium/flash', 'Settings page'), ['admin/settings']),
-                ]
-            ),
-            'email' => Yii::t(
-                'podium/flash', 
-                'No e-mail address has been set for your account! Go to {link} to add one.', 
-                ['link' => Html::a(Yii::t('podium/view', 'Profile') . ' > ' . Yii::t('podium/view', 'Account Details'), ['profile/details'])]
-            ),
-            'old_version' => Yii::t(
-                'podium/flash', 
-                'It looks like there is a new version of Podium database! {link}', 
-                ['link' => Html::a(Yii::t('podium/view', 'Update Podium'), ['install/level-up'])]
-            ),
-            'new_version' => Yii::t(
-                'podium/flash', 
-                'Module version appears to be older than database! Please verify your database.'
-            )
+            'maintenance' => Yii::t('podium/flash', 'Podium is currently in the Maintenance mode. All users without Administrator privileges are redirected to {maintenancePage}. You can switch the mode off at {settingsPage}.', [
+                'maintenancePage' => Html::a(Yii::t('podium/flash', 'Maintenance page'), ['forum/maintenance']),
+                'settingsPage' => Html::a(Yii::t('podium/flash', 'Settings page'), ['admin/settings']),
+            ]),
+            'email' => Yii::t('podium/flash', 'No e-mail address has been set for your account! Go to {link} to add one.', [
+                'link' => Html::a(Yii::t('podium/view', 'Profile') . ' > ' . Yii::t('podium/view', 'Account Details'), ['profile/details'])
+            ]),
+            'old_version' => Yii::t('podium/flash', 'It looks like there is a new version of Podium database! {link}', [
+                'link' => Html::a(Yii::t('podium/view', 'Update Podium'), ['install/level-up'])
+            ]),
+            'new_version' => Yii::t('podium/flash', 'Module version appears to be older than database! Please verify your database.')
         ];
     }
     
@@ -113,23 +98,25 @@ class BaseController extends YiiController
      */
     public function maintenanceCheck($action, $warnings)
     {
-        if ($this->module->podiumConfig->get('maintenance_mode') == '1') {
-            if ($action->id !== 'maintenance') {
-                if ($warnings) {
-                    foreach ($warnings as $warning) {
-                        if ($warning == static::warnings()['maintenance']) {
-                            if (!User::can(Rbac::ROLE_ADMIN)) {
-                                return $this->redirect(['forum/maintenance']);
-                            }
-                            return false;
-                        }
+        if ($this->module->podiumConfig->get('maintenance_mode') != '1') {
+            return false;
+        }
+        if ($action->id === 'maintenance') {
+            return false;
+        }
+        if ($warnings) {
+            foreach ($warnings as $warning) {
+                if ($warning == static::warnings()['maintenance']) {
+                    if (!User::can(Rbac::ROLE_ADMIN)) {
+                        return $this->redirect(['forum/maintenance']);
                     }
-                }
-                $this->warning(static::warnings()['maintenance'], false);
-                if (!User::can(Rbac::ROLE_ADMIN)) {
-                    return $this->redirect(['forum/maintenance']);
+                    return false;
                 }
             }
+        }
+        $this->warning(static::warnings()['maintenance'], false);
+        if (!User::can(Rbac::ROLE_ADMIN)) {
+            return $this->redirect(['forum/maintenance']);
         }
         return false;
     }
@@ -167,7 +154,6 @@ class BaseController extends YiiController
         if (!User::can(Rbac::ROLE_ADMIN)) {
             return false;
         }
-        
         if ($warnings) {
             foreach ($warnings as $warning) {
                 if ($warning == static::warnings()['old_version']) {
@@ -178,7 +164,6 @@ class BaseController extends YiiController
                 }
             }
         }
-        
         $result = Helper::compareVersions(
             explode('.', $this->module->version), 
             explode('.', $this->module->podiumConfig->get('version'))
@@ -188,7 +173,6 @@ class BaseController extends YiiController
         } elseif ($result == '<') {
             $this->warning(static::warnings()['new_version'], false);
         }
-        
         return false;
     }
     
