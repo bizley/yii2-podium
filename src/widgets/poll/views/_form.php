f2ec30157104bff95ab40838758c4ede8d6ca906
Bizley <pawel@positive.codes>
2016-12-13 23:08:31 +0100

    poll refactor

---
+++ src/widgets/poll/views/_form.php
@@ -0,0 +1,61 @@
+<?php
+
+use kartik\date\DatePicker;
+
+$answers = 0;
+$options = [];
+for ($a = 1; $a <= 10; $a++) {
+    $opts = ['placeholder' => Yii::t('podium/view', 'Leave empty to remove')];
+    $opts['id'] = 'thread-poll_answers' . ($a > 1 ? '_' . $a : '');
+    if (!empty($model->$pollAnswers[$a - 1])) {
+        $opts['value'] = $model->$pollAnswers[$a - 1];
+        $answers++;
+    } else {
+        $opts['value'] = null;
+    }
+    $options[$a] = $opts;
+}
+$answers = max([2, $answers]);
+$this->registerJs(<<<JS
+var answers = $answers; $(".podium-poll-plus button").click(function(e) { e.preventDefault(); answers++; if ($(".podium-poll-opt-" + answers).length) { $(".podium-poll-opt-" + answers).removeClass("hide"); } if (answers >= 10) { $(".podium-poll-plus").addClass("hide"); }});
+JS
+);
+
+$fieldLayoutLong = [
+    'labelOptions' => ['class' => 'control-label col-sm-3'], 
+    'template' => "{label}\n<div class=\"col-sm-9\">{input}\n{hint}\n{error}</div>"
+];
+$fieldLayoutShort = [
+    'labelOptions' => ['class' => 'control-label col-sm-3'], 
+    'template' => "{label}\n<div class=\"col-sm-3\">{input}\n{hint}\n{error}</div>"
+];
+
+?>
+<div class="row">
+    <?= $form->field($model, $pollQuestion, $fieldLayoutLong); ?>
+</div>
+<div class="row">
+    <?= $form->field($model, $pollVotes, $fieldLayoutShort); ?>
+</div>
+<div class="row">
+    <?= $form->field($model, $pollHidden, [
+        'checkboxTemplate' => "<div class=\"col-sm-offset-3 col-sm-9\">\n{beginLabel}\n{input}\n{labelTitle}\n{endLabel}\n{error}\n{hint}\n</div>"
+    ])->checkbox(); ?>
+</div>
+<div class="row">
+    <?= $form->field($model, $pollEnd, $fieldLayoutShort)->widget(DatePicker::classname(), [
+        'removeButton' => false, 'pluginOptions' => ['autoclose' => true, 'format' => 'yyyy-mm-dd']
+    ]); ?>
+</div>
+<?php foreach ($options as $index => $option): ?>
+<div class="row <?= $index > 2 ? 'podium-poll-opt-' . $index : '' ?> <?= $option['value'] === null && $index > 2 ? 'hide' : '' ?>">
+    <?= $form->field($model, $pollAnswers .'[]', $fieldLayoutLong)
+            ->label(Yii::t('podium/view', 'Option #{n}', ['n' => $index]), ['for' => $option['id']])
+            ->textInput($option); ?>
+</div>
+<?php endforeach; ?>
+<div class="row podium-poll-plus">
+    <div class="col-sm-offset-3 col-sm-9">
+        <button class="btn btn-default btn-xs"><span class="glyphicon glyphicon-plus"></span> <?= Yii::t('podium/view', 'One more'); ?></button>
+    </div>
+</div>
--- src/widgets/poll/views/create.php
+++ src/widgets/poll/views/create.php
@@ -1,32 +1,12 @@
 <?php
 
-use kartik\date\DatePicker;
 use yii\helpers\Html;
 
 $this->registerJs(<<<JS
-$(".podium-poll-add").click(function(e) {
-    e.preventDefault();
-    $(".new-poll").removeClass("hide");
-    $("#poll_added").val(1);
-    $(this).addClass("hide");
-});
-$(".podium-poll-discard").click(function(e) {
-    e.preventDefault();
-    $(".new-poll").addClass("hide");
-    $("#poll_added").val(0);
-    $(".podium-poll-add").removeClass("hide");
-});
+$(".podium-poll-add").click(function(e) { e.preventDefault(); $(".new-poll").removeClass("hide"); $("#poll_added").val(1); $(this).addClass("hide"); });
+$(".podium-poll-discard").click(function(e) { e.preventDefault(); $(".new-poll").addClass("hide"); $("#poll_added").val(0); $(".podium-poll-add").removeClass("hide"); });
 JS
 );
-
-$fieldLayoutLong = [
-    'labelOptions' => ['class' => 'control-label col-sm-3'], 
-    'template' => "{label}\n<div class=\"col-sm-9\">{input}\n{hint}\n{error}</div>"
-];
-$fieldLayoutShort = [
-    'labelOptions' => ['class' => 'control-label col-sm-3'], 
-    'template' => "{label}\n<div class=\"col-sm-3\">{input}\n{hint}\n{error}</div>"
-];
 
 echo Html::activeHiddenInput($model, 'pollAdded', ['id' => 'poll_added']);
 ?>
@@ -41,58 +21,15 @@ echo Html::activeHiddenInput($model, 'pollAdded', ['id' => 'poll_added']);
             <strong><?= Yii::t('podium/view', 'New poll'); ?></strong>
         </div>
         <div class="panel-body">
-            <div class="row">
-                <?= $form->field($model, 'pollQuestion', $fieldLayoutLong); ?>
-            </div>
-            <div class="row">
-                <?= $form->field($model, 'pollVotes', $fieldLayoutShort); ?>
-            </div>
-            <div class="row">
-                <?= $form->field($model, 'pollHidden', [
-                    'checkboxTemplate' => "<div class=\"col-sm-offset-3 col-sm-9\">\n{beginLabel}\n{input}\n{labelTitle}\n{endLabel}\n{error}\n{hint}\n</div>"
-                ])->checkbox(); ?>
-            </div>
-            <div class="row">
-                <?= $form->field($model, 'pollEnd', $fieldLayoutShort)->widget(DatePicker::classname(), [
-                    'removeButton' => false, 'pluginOptions' => ['autoclose' => true, 'format' => 'yyyy-mm-dd']
-                ]); ?>
-            </div>
-<?php $opts = ['placeholder' => Yii::t('podium/view', 'Leave empty to remove')]; ?>
-<?php $answers = 0; for ($a = 1; $a <= 10; $a++):
-    $opts['id'] = 'thread-poll_answers' . ($a > 1 ? '_' . $a : '');
-    if (!empty($model->pollAnswers[$a - 1])):
-        $opts['value'] = $model->pollAnswers[$a - 1];
-        $answers++;
-    else:
-        $opts['value'] = null;
-    endif; ?>
-            <div class="row <?= $a > 2 ? 'podium-poll-opt-' . $a : '' ?> <?= $opts['value'] === null && $a > 2 ? 'hide' : '' ?>">
-                <?= $form->field($model, 'pollAnswers[]', $fieldLayoutLong)
-                        ->label(Yii::t('podium/view', 'Option #{n}', ['n' => $a]), ['for' => $opts['id']])
-                        ->textInput($opts); ?>
-            </div>
-<?php endfor; ?>
-            <div class="row podium-poll-plus">
-                <div class="col-sm-offset-3 col-sm-9">
-                    <button class="btn btn-default btn-xs"><span class="glyphicon glyphicon-plus"></span> <?= Yii::t('podium/view', 'One more'); ?></button>
-                </div>
-            </div>
+            <?= $this->render('_form', [
+                'form' => $form,
+                'model' => $model,
+                'pollQuestion' => 'pollQuestion',
+                'pollVotes' => 'pollVotes',
+                'pollHidden' => 'pollHidden',
+                'pollEnd' => 'pollEnd',
+                'pollAnswers' => 'pollAnswers',
+            ]) ?>
         </div>
     </div>
 </div>
-<?php
-$answers = max([2, $answers]);
-$this->registerJs(<<<JS
-var answers = $answers;
-$(".podium-poll-plus button").click(function(e) {
-    e.preventDefault();
-    answers++;
-    if ($(".podium-poll-opt-" + answers).length) {
-        $(".podium-poll-opt-" + answers).removeClass("hide");
-    }
-    if (answers >= 10) {
-        $(".podium-poll-plus").addClass("hide");
-    }
-});
-JS
-);
